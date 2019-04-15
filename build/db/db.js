953415656144e4c155e4d565c4939c0510415af1
RaunG <raung@icloud.com>
2019-04-15 08:25:59 -0500

    Update db.js

--- build/db/db.js
+++ build/db/db.js
@@ -7,17 +7,9 @@ const pg_promise_1 = __importDefault(require("pg-promise"));
 exports.pgp = pg_promise_1.default();
 const dbConf = {
     host: '127.0.0.1',
-<<<<<<< HEAD
     port: 5432,
     database: 'tourdb',
     user: 'postgres',
-=======
-    // Use port 5432 for work; port 5433 for personal laptop
-    // port: 5432,
-    port: 5433,
-    database: 'tourdb',
-    user: 'tourdb_admin',
->>>>>>> a77c9ef3c8872825fbc687bdf0875a7258442897
     password: 'maurice'
 };
 exports.db = exports.pgp(dbConf);
